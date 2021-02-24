bb6eb5fef982eb2e9c812e19dd25b9f640e3bfb9
Anderson Conforto <afcaconforto@gmail.com>
2021-02-24 10:54:29 -0300

    Exercicio loja de games

---
+++ gamesShow/src/main/java/com/lojadegames/gamesShow/GamesShowApplication.java
@@ -0,0 +1,13 @@
+package com.lojadegames.gamesShow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesShowApplication.class, args);
+	}
+
+}
